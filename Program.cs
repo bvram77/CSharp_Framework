0a2fe5cdb5038a42c719d21b83ed5cde61a57af6
VENKATRAMAN <bvram77@hotmail.com>
2023-03-02 06:57:41 +0530

    properties updates

--- Program.cs
+++ Program.cs
@@ -2,11 +2,21 @@
 
 namespace ConsoleApp1
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            String applicaton = initProp();
+            Console.WriteLine(applicaton);
+
+        }
+
+        public static string initProp()
+        {
+            var Properties = new IProperties(@"D:\Venkat-Freelancing-Project\Csharp-Framework\Selenium-Framework\GlobalSettings.config");
+            String uname = Properties.Get("ProjectName");
+            return uname;
         }
     }
 }
